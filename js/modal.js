8239445cde52f200cca241eea90615c627986b85
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-12-10 21:10:44 +0200

    Correct method to get modal button elements
    Fix method name for selecting modal buttons.

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
+const modalBtnOpen = document.getElementByClass('modal-btn-open');
+const modalBtnClose = document.getElementByClass('modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
