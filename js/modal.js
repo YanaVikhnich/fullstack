663d0e179fc63ac64654c7865e26a6df58676105
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-12-11 14:48:51 +0200

    changes in the js file getElementByClass => querySelectAll

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.getElementByClass('modal-btn-open');
-const modalBtnClose = document.getElementByClass('modal-btn-close');
+const modalBtnOpen = document.querySelectAll('.modal-btn-open');
+const modalBtnClose = document.querySelectAll('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
